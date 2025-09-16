be07e1d6cdd644ad820c7fb822e4c3450918a7d8
Artniaina <kanto.andriahariniaina@gmail.com>
2025-09-16 16:37:35 +0300

    Feat: add homepage component

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,4 +1,14 @@
+import Activites from "../components/Accueil/Activites/Activites";
+import Engagements from "../components/Accueil/Engagements/Engagements";
+import HeroSection from "../components/Accueil/HeroSection/HeroSection";
+
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <HeroSection />
+      <Activites />
+      <Engagements />
+    </div>
+  );
 };
 export default Homepage;
