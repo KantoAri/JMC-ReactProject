95fa808a35348306581dab891a04d3d5f30cb765
Artniaina <kanto.andriahariniaina@gmail.com>
2025-09-23 10:16:17 +0300

    feat: finalize legal mentions responsive design

--- src/components/MentionLegales/MentionLegales.jsx
+++ src/components/MentionLegales/MentionLegales.jsx
@@ -1,11 +1,14 @@
 import backgroundImg from "../../assets/mention-legale-1024x682.webp";
 import { useState, useEffect } from "react";
-import { FaPhoneSquareAlt } from "react-icons/fa";
+import {
+  FaPhoneSquareAlt,
+  FaFileAlt,
+  FaMapMarkedAlt,
+  FaLaptopHouse,
+} from "react-icons/fa";
 import { IoMdMail } from "react-icons/io";
-import { FaFileAlt } from "react-icons/fa";
-import { FaMapMarkedAlt } from "react-icons/fa";
 import { BsMenuButtonWide } from "react-icons/bs";
-import { FaLaptopHouse } from "react-icons/fa";
+import "./MentionLegales.css";
 
 const MentionLegales = () => {
   const [animationComplete, setAnimationComplete] = useState(false);
@@ -15,120 +18,89 @@ const MentionLegales = () => {
   }, []);
 
   const iconSize = 50;
-  const editeurColor = "#61d8ebff";
-  const hebergeurColor = "#ff8c00";
 
   return (
     <>
-      <div
-        className="container-fluid hello position-relative pt-3"
-        style={{
-          backgroundImage: ` linear-gradient(to right, rgba(52, 148, 206, 0.52), rgba(46, 206, 255, 0.5)), url(${backgroundImg})`,
-          backgroundSize: "100% 110%",
-          minHeight: "60vh",
-          backgroundRepeat: "no-repeat",
-        }}
-      >
-        <h1
-          className="fw-bold position-relative text-white"
-          style={{
-            paddingBottom: "20px",
-            fontSize: "5rem",
-            fontSize: "clamp(3.2rem, 8vw, 4rem)",
-            marginTop: "clamp(10rem, 5vw, 20rem)",
-            marginLeft: "12rem",
-          }}
-        >
-          Mentions légales{" "}
-          <span
-            style={{
-              position: "absolute",
-              bottom: 0,
-              left: 0,
-              width: "8rem",
-              height: "5px",
-              backgroundColor: "#ffffffff",
-              width: "clamp(4rem, 20vw, 8rem)",
-            }}
-          ></span>
+      <div className="mention-hero  position-relative pt-3 ">
+        <h1 className="mention-title fw-bold position-relative text-white text-md-start ">
+          Mentions légales
+          <span className="underline-desktop d-none d-md-block"></span>
+          <span className="underline-mobile d-md-none d-block "></span>
         </h1>
       </div>
-      <div
-        className="container-fluid py-5"
-        style={{
-          backgroundColor: "#f8f9fa",
-          borderTop: "15px solid #0084ffff",
-          borderBottom: "15px solid #0084ffff",
-          padding: "0 12rem",
-        }}
-      >
-        <div className="row">
-          <div className="col-md-6 pe-md-4">
-            <div className="bg-white p-4 h-100 border-end border-primary border-4">
-              <h2
-                className=" fw-bold mb-5"
-                style={{ fontSize: "3rem", color: editeurColor }}
-              >
+
+      <div className="mention-content container-fluid py-5 px-md-5 px-3">
+        <div
+          className="row content-row"
+          style={{ width: "83%", margin: "auto" }}
+        >
+          <div className="col-md-6 pe-md-4 mb-5 mb-md-0">
+            <div className="editor-box">
+              <h2 className="section-title fw-bold mb-5 text-center text-md-start editor-color">
                 L'EDITEUR
               </h2>
 
-              <div className="mb-4 d-flex align-items-start">
-                <div className="me-3">
-                  <BsMenuButtonWide color={editeurColor} size={iconSize} />
+              <div className="mb-4 d-flex flex-md-row flex-column text-center text-md-start info-row">
+                <div className="me-md-3 mb-3 mb-md-0 d-flex justify-content-center icon">
+                  <FaLaptopHouse color="#21b7ceff" size={iconSize} />
                 </div>
                 <div>
-                  <strong className="text-secondary">JM-CONTACTS SARL</strong>
+                  <strong className="text-secondary info-text">
+                    JM-CONTACTS SARL
+                  </strong>
                 </div>
               </div>
 
-              <div className="mb-4 d-flex align-items-start">
-                <div className="me-3">
-                  <FaMapMarkedAlt color={editeurColor} size={iconSize} />
+              <div className="mb-4 d-flex flex-md-row flex-column text-center text-md-start info-row">
+                <div className="me-md-3 mb-3 mb-md-0 d-flex justify-content-center icon">
+                  <FaMapMarkedAlt color="#21b7ceff" size={iconSize} />
                 </div>
                 <div>
-                  <span className="text-secondary">
+                  <span className="text-secondary info-text">
                     <strong>Lot 054 B Ter Immeuble Nirina 29, Ambohibao</strong>
                   </span>
                 </div>
               </div>
 
-              <div className="mb-4 d-flex align-items-start">
-                <div className="me-3">
-                  <FaPhoneSquareAlt color={editeurColor} size={iconSize} />
+              <div className="mb-4 d-flex flex-md-row flex-column text-center text-md-start info-row">
+                <div className="me-md-3 mb-3 mb-md-0 d-flex justify-content-center icon">
+                  <FaPhoneSquareAlt color="#21b7ceff" size={iconSize} />
                 </div>
                 <div>
-                  <span className="text-secondary">
+                  <span className="text-secondary info-text">
                     <strong>02 44 10 01 02 ou (+ 261) 32 05 369 00</strong>
                   </span>
                 </div>
               </div>
 
-              <div className="mb-4 d-flex align-items-start">
-                <div className="me-3">
-                  <IoMdMail color={editeurColor} size={iconSize} />
+              <div className="mb-4 d-flex flex-md-row flex-column text-center text-md-start info-row">
+                <div className="me-md-3 mb-3 mb-md-0 d-flex justify-content-center icon">
+                  <IoMdMail color="#21b7ceff" size={iconSize} />
                 </div>
                 <div>
-                  <span className="text-secondary">
+                  <span className="text-secondary info-text">
                     <strong>j.morvan@jm-contacts.com</strong>
                   </span>
                 </div>
               </div>
 
-              <div className="mb-4 d-flex align-items-start">
-                <div className="me-3">
-                  <FaFileAlt color={editeurColor} size={iconSize} />
+              <div className="mb-4 d-flex flex-md-row flex-column text-center text-md-start info-row">
+                <div className="me-md-3 mb-3 mb-md-0 d-flex justify-content-center icon">
+                  <FaFileAlt color="#21b7ceff" size={iconSize} />
                 </div>
                 <div>
-                  <div>
-                    <strong className="text-secondary">
-                      Numéros d'enregistrements administratifs et fiscaux:
-                    </strong>
-                    <div className="mt-2">
-                      <div className="text-secondary">NIF : 5000 428 852</div>
-                      <div className="text-secondary">
-                        STAT : 46512 11 2000 0 10158
-                      </div>
-                      <div className="text-secondary">RCS : 2000B00101</div>
+                  <strong className="text-secondary info-text">
+                    Numéros d'enregistrements administratifs et fiscaux:
+                  </strong>
+                  <div className="mt-2">
+                    <div className="text-secondary info-text">
+                      NIF : 5000 428 852
+                    </div>
+                    <div className="text-secondary info-text">
+                      STAT : 46512 11 2000 0 10158
+                    </div>
+                    <div className="text-secondary info-text">
+                      RCS : 2000B00101
                     </div>
                   </div>
                 </div>
@@ -137,74 +109,69 @@ const MentionLegales = () => {
           </div>
 
           <div className="col-md-6 ps-md-4">
-            <div className="bg-white p-4 h-100">
-              <h2
-                className="fw-bold mb-5"
-                style={{ fontSize: "3rem", color: hebergeurColor }}
-              >
+            <div className="hebergeur-box bg-white p-4 h-100">
+              <h2 className="section-title fw-bold mb-5 text-start text-md-start hebergeur-color">
                 L'HEBERGEUR
               </h2>
 
-              <div className="mb-4 d-flex align-items-start">
-                <div className="me-3">
-                  <FaLaptopHouse color={hebergeurColor} size={iconSize} />
+              <div className="mb-4 d-flex flex-md-row flex-column text-center text-md-start info-row">
+                <div className="me-md-3 mb-3 mb-md-0 d-flex justify-content-center icon">
+                  <BsMenuButtonWide color="#ff8c00" size={iconSize} />
                 </div>
                 <div>
-                  <strong className="text-secondary">
+                  <strong className="text-secondary info-text">
                     o2switch-PowerBoost-v3
                   </strong>
                 </div>
               </div>
 
-              <div className="mb-4 d-flex align-items-start">
-                <div className="me-3">
-                  <FaMapMarkedAlt color={hebergeurColor} size={iconSize} />
+              <div className="mb-4 d-flex flex-md-row flex-column text-center text-md-start info-row">
+                <div className="me-md-3 mb-3 mb-md-0 d-flex justify-content-center icon">
+                  <FaMapMarkedAlt color="#ff8c00" size={iconSize} />
                 </div>
                 <div>
-                  <span className="text-secondary">
+                  <span className="text-secondary info-text">
                     <strong>Chemin des Pardiaux 63000 Clermont-Ferrand</strong>
                   </span>
                 </div>
               </div>
 
-              <div className="mb-4 d-flex align-items-start">
-                <div className="me-3">
-                  <FaPhoneSquareAlt color={hebergeurColor} size={iconSize} />
+              <div className="mb-4 d-flex flex-md-row flex-column text-center text-md-start info-row">
+                <div className="me-md-3 mb-3 mb-md-0 d-flex justify-content-center icon">
+                  <FaPhoneSquareAlt color="#ff8c00" size={iconSize} />
                 </div>
                 <div>
-                  <span className="text-secondary">
+                  <span className="text-secondary info-text">
                     <strong>0444446040</strong>
                   </span>
                 </div>
               </div>
 
-              <div className="mb-4 d-flex align-items-start">
-                <div className="me-3">
-                  <IoMdMail color={hebergeurColor} size={iconSize} />
+              <div className="mb-4 d-flex flex-md-row flex-column text-center text-md-start info-row">
+                <div className="me-md-3 mb-3 mb-md-0 d-flex justify-content-center icon">
+                  <IoMdMail color="#ff8c00" size={iconSize} />
                 </div>
                 <div>
-                  <span className="text-secondary">
+                  <span className="text-secondary info-text">
                     <strong>support@o2switch.fr</strong>
                   </span>
                 </div>
               </div>
 
-              <div className="mb-4 d-flex align-items-start">
-                <div className="me-3">
-                  <FaFileAlt color={hebergeurColor} size={iconSize} />
+              <div className="mb-4 d-flex flex-md-row flex-column text-center text-md-start info-row">
+                <div className="me-md-3 mb-3 mb-md-0 d-flex justify-content-center icon">
+                  <FaFileAlt color="#ff8c00" size={iconSize} />
                 </div>
                 <div>
-                  <div>
-                    <strong className="text-secondary">
-                      Numéros d'enregistrements administratifs et fiscaux:
-                    </strong>
-                    <div className="mt-2">
-                      <div className="text-secondary">
-                        SIRET : 510 909 80700024
-                      </div>
-                      <div className="text-secondary">
-                        RCS : Clermont-Ferrand
-                      </div>
+                  <strong className="text-secondary info-text">
+                    Numéros d'enregistrements administratifs et fiscaux:
+                  </strong>
+                  <div className="mt-2">
+                    <div className="text-secondary info-text">
+                      SIRET : 510 909 80700024
+                    </div>
+                    <div className="text-secondary info-text">
+                      RCS : Clermont-Ferrand
                     </div>
                   </div>
                 </div>
